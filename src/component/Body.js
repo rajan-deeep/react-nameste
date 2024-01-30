5e0fe7b8dc815587980810d518cf759c22067df1
rajan-deeep <134048013+rajan-deeep@users.noreply.github.com>
2024-01-31 00:20:20 +0530

    changes in body before episode 6

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,12 +1,25 @@
 import {restrautList} from "../utils/mockData"
 import RestrauntCard from "./RestrauntCard";
+import { useState } from "react";
 
 const Body = () => {
+    const [restaurantListState,setRestaurantList] = useState(restrautList);
     return (
+      <div>
+      <button id="btn" onClick={()=>{
+        const list=restaurantListState.filter(
+          (res)=> res.data.avgRating > 4);
+        setRestaurantList(list);
+        console.log(restaurantListState);
+      }}
+      >
+        Top rated resturant
+      </button>
       <div className="restaurant-list">
-        {restrautList.map((restaurant) => {
+        {restaurantListState.map((restaurant) => {
           return <RestrauntCard {...restaurant.data} key={restaurant.data.id} />;
         })}
+      </div>
       </div>
     );
   };
