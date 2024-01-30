5857d03f0a52f120b59c42a071e8ec7e29970f63
rajan-deeep <134048013+rajan-deeep@users.noreply.github.com>
2024-01-31 01:25:46 +0530

    adding usage of search using another usestate var

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,22 +1,55 @@
 import {restrautList} from "../utils/mockData"
 import RestrauntCard from "./RestrauntCard";
-import { useState } from "react";
+import Shimmer from "./Shimmer";
+import { useEffect, useState } from "react";
 
 const Body = () => {
     const [restaurantListState,setRestaurantList] = useState(restrautList);
-    return (
+    const [filteredRes,setFilteredRes] = useState(restrautList);
+    const [searchText,setSearchText] = useState("");
+
+    // useEffect(()=>{
+    //   fetchData();
+    // },[]);
+
+
+    //todo api call to swiggy or zomata
+    // const fetchData = async()=>{
+    //   const data  = await fetch();
+    //   const json = await data.json();
+
+    //   setRestaurantList(json.data.cards[2].data.data.cards);
+    //   setFilteredRes(json.data.cards[2].data.data.cards);
+    // }
+
+    return restaurantListState.length===0?<Shimmer/>:(
       <div>
       <button id="btn" onClick={()=>{
         const list=restaurantListState.filter(
           (res)=> res.data.avgRating > 4);
-        setRestaurantList(list);
+          setFilteredRes(list);
         console.log(restaurantListState);
       }}
       >
         Top rated resturant
       </button>
+      <div className="search">
+        <input type="text" className="search-box" value={searchText} onChange={(e)=>{
+          console.log(e.target.value);
+          setSearchText(e.target.value)
+        }}>
+        </input>
+        <button onClick={()=>{
+          console.log(restaurantListState);
+          const filtered=restaurantListState.filter(res=>res.data.name.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()));
+          console.log(filtered);
+          setFilteredRes(filtered);
+        }}>
+          Search
+        </button>
+      </div>
       <div className="restaurant-list">
-        {restaurantListState.map((restaurant) => {
+        {filteredRes.map((restaurant) => {
           return <RestrauntCard {...restaurant.data} key={restaurant.data.id} />;
         })}
       </div>
